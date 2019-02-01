1c5a61e8312c1e386ed5613e1cd94204e614a1af
Devon Bacon <15711397+devbacon@users.noreply.github.com>
2019-01-31 20:32:24 -0800

    expand Dice.roll()

--- Dice.java
+++ Dice.java
@@ -2,12 +2,12 @@ import java.lang.Math;
 
 public class Dice {
   public static void main(String[] args) {
-    System.out.println(testRand());
+    System.out.println(roll(20));
   }
 
-  // public static int roll(int sides) {
-    
-  // }
+  public static int roll(int sides) {
+    return (int) Math.random() * sides + 1;
+  }
 
   public static double testRand() {
     return Math.random();
