4f33dd6c7d188dc72896ebdfa180c4597826706e
Devon Bacon <15711397+devbacon@users.noreply.github.com>
2019-01-31 20:15:08 -0800

    add print to Dice main method

--- Dice.java
+++ Dice.java
@@ -2,14 +2,14 @@ import java.lang.Math;
 
 public class Dice {
   public static void main(String[] args) {
-
+    System.out.println(testRand());
   }
 
   // public static int roll(int sides) {
     
   // }
 
-  public static void testRand() {
-    System.out.println(Math.random());
+  public static int testRand() {
+    return Math.random();
   }
 }
