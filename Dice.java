21637f65ddf9b9eed81f9d161fb779c672056e65
Devon Bacon <15711397+devbacon@users.noreply.github.com>
2019-01-31 21:28:30 -0800

    add doc comments

--- Dice.java
+++ Dice.java
@@ -1,12 +1,21 @@
+/**
+ * This class is a dice utility for tabletop board games.
+ * It takes in dice roll data and return a randomized result within a calculated range.
+ */
 public class Dice {
   public static void main(String[] args) {
 
   }
 
-  /*
-    Take in number of dice to be rolled and sides on each dice
-      (5, 6) => Totals up 5 rolls of a 6 sides dice
-  */
+  /**
+   * This method takes in two whole numbers.
+   * These numbers represent how many dice you would like to roll and how many sides each dice has.
+   * The return value is a randomized total of that dice roll.
+   * 
+   * @param numOfDice number of dice to be rolled
+   * @param sidesOnDice number of sides on each dice
+   * @return random number between minimum and maximum roll result
+   */
   public static int roll(int numOfDice, int sidesOnDice) {
     int max = numOfDice * sidesOnDice;
     int min = numOfDice;
